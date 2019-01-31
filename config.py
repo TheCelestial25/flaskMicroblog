7c48dff2ac1e4b6b3b9afcccdfb95ab083bc45f4
Priyanshu Singh <37958393+TheCelestial25@users.noreply.github.com>
2019-01-31 23:35:48 +0530

    Update config.py
    Changed env settings

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
     MAIL_USERNAME = 'celestial25.dev@gmail.com'
-    MAIL_PASSWORD = 'nobruteforce8'
+    MAIL_PASSWORD = '' #hidden
     ADMINS = ['celestial25.dev@gmail.com']
     POSTS_PER_PAGE = 25
